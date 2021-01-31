6957400eea61f1f971170153967453c8cc99d9d3
Olha Halat <olha.halat@gmail.com>
2021-01-31 01:52:47 +0100

    add registration and login forms

--- flaskjournal/flaskjournal.py
+++ flaskjournal/flaskjournal.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
+
+app.config['SECRET_KEY'] = '67ad1fad9b01f8012b84e740919bb81d'
 
 posts = [
 	{
@@ -24,11 +28,25 @@ posts = [
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', posts=posts)
+	return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About')
+	return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form = form)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Log in', form = form)
+
 
 # this allows to run the app with 'python <filename>' instead of first setting up the env. variable with 'export FLASK_APP=flaskjournal.py' and running the app with 'flask run'
 if __name__ == '__main__':
